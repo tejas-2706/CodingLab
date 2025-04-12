88e7f732c44a583724b2eb3c24c1dfba92783149
tejas <pangaonkar.svc424@gmail.com>
2025-04-12 18:12:23 +0530

    added DSA compiler lab in progress

--- Frontend/src/components/DsaExecution.tsx
+++ Frontend/src/components/DsaExecution.tsx
@@ -1,58 +1,147 @@
-import React from 'react';
+import { Editor } from "@monaco-editor/react";
+import React from "react";
+import QuestionArea from "./QuestionArea";
 
-const DsaExecution: React.FC = () => {
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsYWJJZCI6IjY3ZjhiYmY3OTFmMjdiNDNiNjdlZDkyNiIsInVzZXJJZCI6IjEyMzQiLCJpYXQiOjE3NDQzNTUxMjAsImV4cCI6MTc0NDM1ODcyMH0.V5MaGzPvVBuw74L9Qnzq_UcEaLoaf_MnboN4qAyWwYg'
+function DsaExecution() {
+  const [code, setCode] = React.useState(
+    '#include <stdio.h>\nvoid main(){\nprintf("h");\n}'
+  );
+  const [taskId, setTaskId] = React.useState("");
+  const [output, setOutput] = React.useState("");
+  // const apiKey = import.meta.env.FERMION_API_KEY;
+  React.useEffect(() => console.log(taskId), [taskId]);
+
+  const handleCodeRun = () => {
+    fetch(
+      "https://backend.codedamn.com/api/public/request-dsa-code-execution",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "FERMION-API-KEY": "",
+        },
+        body: JSON.stringify({
+          data: [
+            {
+              data: {
+                language: "C",
+                runConfig: {
+                  customMatcherToUseForExpectedOutput: "ExactMatch",
+                  expectedOutputAsBase64UrlEncoded: btoa("h")
+                    .replace(/\+/g, "-")
+                    .replace(/\//g, "_")
+                    .replace(/=+$/, ""),
+                  stdinStringAsBase64UrlEncoded: "",
+                  // callbackUrlOnExecutionCompletion: "",
+                  shouldEnablePerProcessAndThreadCpuTimeLimit: false,
+                  shouldEnablePerProcessAndThreadMemoryLimit: false,
+                  shouldAllowInternetAccess: false,
+                  compilerFlagString: "",
+                  maxFileSizeInKilobytesFilesCreatedOrModified: 1024,
+                  stackSizeLimitInKilobytes: 65536,
+                  cpuTimeLimitInMilliseconds: 2000,
+                  wallTimeLimitInMilliseconds: 5000,
+                  memoryLimitInKilobyte: 131072,
+                  maxProcessesAndOrThreads: 60,
+                },
+                sourceCodeAsBase64UrlEncoded: btoa(code)
+                  .replace(/\+/g, "-")
+                  .replace(/\//g, "_")
+                  .replace(/=+$/, ""),
+              },
+            },
+          ],
+        }),
+      }
+    )
+      .then((res) => res.json())
+      .then((body) => setTaskId(body[0].output.data.taskId));
+  };
+
+  const handleViewStatus = () => {
+    fetch(
+      "https://backend.codedamn.com/api/public/get-dsa-code-execution-result",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "FERMION-API-KEY": "",
+        },
+        body: JSON.stringify({
+          data: [
+            {
+              data: {
+                taskUniqueId: taskId,
+              },
+            },
+          ],
+        }),
+      }
+    )
+      .then((res) => res.json())
+      .then((body) => {
+        let data = body[0].output.data.runResult;
+        console.log(data);
+        let err = data.programRunData.stderrBase64UrlEncoded
+          .replace(/-/g, "+")
+          .replace(/_/g, "/");
+        console.log(atob(err));
+        let output = data.programRunData.stdoutBase64UrlEncoded
+          .replace(/-/g, "+")
+          .replace(/_/g, "/");
+        console.log("o/p - ", atob(output));
+        setOutput(atob(output))
+      });
+  };
+
   return (
-    <div className="relative w-full h-[600px] overflow-hidden rounded-md border border-gray-200">
-      <iframe
-        width="100%"
-        height="100%"
-        src={`https://edudiagno.fermion.app/embed/io-coding-lab?token=${token}`}
-        title="Coding Lab"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen
-        className="absolute top-0 left-0"
-      />
+    <div className="bg-[#18181b]">
+      <div className="flex justify-center gap-6 p-2">
+        <button className="bg-green-400 px-4 rounded-xl" onClick={handleCodeRun}>Run</button>
+        <button className="bg-white px-4 text-black rounded-xl" onClick={handleViewStatus}>View Status</button>
+      </div>
+      <div className="grid md:grid-cols-2 grid-cols-1">
+        <div>
+          <QuestionArea />
+        </div>
+        <div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              width: "100%",
+              gap: "1rem",
+            }}
+          >
+          </div>
+          {/* <textarea
+        className="bg-gray-300"
+        style={{ width: "100%", height: "360px" }}
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+      ></textarea> */}
+          <Editor
+            height="360px"
+            width="100%"
+            language="c"
+            value={code}
+            onChange={(value) => setCode(value || '')}
+            theme="vs-dark"
+            options={{
+              minimap: { enabled: false },
+              fontSize: 14,
+              scrollBeyondLastLine: false,
+              automaticLayout: true,
+            }}
+          />
+          <div className="p-4 bg-[#27272a] mt-4 rounded-xl text-white">
+            <h1>Output - </h1>
+            {output}
+          </div>
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default DsaExecution;
-
-
-
-
-
-
-// export const DsaExecution: React.FC = () => {
-//   const [isLoading, setIsLoading] = React.useState(true);
-//   const [error, setError] = React.useState<string | null>(null);
-//   const token = '67f8bbf791f27b43b67ed926';
-
-//   const handleIframeError = () => {
-//     setError('Failed to load the coding lab.');
-//     setIsLoading(false);
-//   };
-
-//   return (
-//     <div className="relative w-full h-[600px] overflow-hidden rounded-md border border-gray-200">
-//       {isLoading && <div>Loading...</div>}
-//       {error && <div className="text-red-500">{error}</div>}
-//       <iframe
-//         width="100%"
-//         height="100%"
-//         src={`https://edudiagno.fermion.app/embed/io-coding-lab?token=${token}`}
-//         title="Coding Lab"
-//         frameBorder="0"
-//         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-//         referrerPolicy="strict-origin-when-cross-origin"
-//         allowFullScreen
-//         className="absolute top-0 left-0"
-//         onLoad={() => setIsLoading(false)}
-//         onError={handleIframeError}
-//       />
-//     </div>
-//   );
-// };
---
+++ Frontend/src/components/QuestionArea.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function QuestionArea() {
+  return (
+    <div className='bg-[#27272a] rounded-xl text-white'>
+        QuestionArea
+    </div>
+  )
+}
+
+export default QuestionArea
